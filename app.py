2112293f8eccac4b17d8478ed605a373d13c0d43
gsh1991 <115839918+gsh1991@users.noreply.github.com>
2022-11-11 18:34:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 import pickle
 
-app=Flask(__name__,template_folder='templates')
+app=Flask(__name__,template_folder='templates',static_url_path='/static')
 model = pickle.load(open('model1.pkl', 'rb'))
 
 @app.route('/')
